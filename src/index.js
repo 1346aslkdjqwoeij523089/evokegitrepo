1d9cc035dbb452b9592bc38bf8f29f339baf3e29
manbozo5235235124 <pyradex1@gmail.com>
2026-06-30 22:25:22 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`${client.user?.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
