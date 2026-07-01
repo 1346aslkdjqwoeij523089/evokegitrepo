f78639e85969365759542e12cfdee4d5b91615dc
manbozo5235235124 <pyradex1@gmail.com>
2026-06-30 22:41:27 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import 'dotenv/config';
-import { Client, GatewayIntentBits } from 'discord.js';
+import "dotenv/config";
+import { Client, GatewayIntentBits } from "discord.js";
+import { createKazagumo } from "./music/lavalink";
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildVoiceStates
-    ]
+        GatewayIntentBits.GuildVoiceStates,
+    ],
 });
 
-client.once('ready', () => {
+client.once("ready", () => {
     console.log(`${client.user?.tag} is online!`);
+
+    // INIT MUSIC SYSTEM
+    createKazagumo(client);
 });
 
 client.login(process.env.TOKEN);
